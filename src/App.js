d66ca50c47b192dc381026b237ce743982585cdf
aslanka <ayushlanka106@gmail.com>
2024-09-13 16:14:56 -0400

    register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/login';
-
+import Register from './pages/register/register';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/register/register.js
@@ -0,0 +1,10 @@
+
+import React, { useState } from "react";
+
+const Register = () => {
+<div>
+    <h1>Register</h1>
+</div>
+}
+
+export default Register
